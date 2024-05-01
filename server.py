c9945a807fff264f8cf91fa7cbe5ccff7a595876
Sasha <alexalexbor04@gmail.com>
2024-05-01 19:45:11 +0300

    new branch and serv

--- server.py
+++ server.py
@@ -1,20 +1,71 @@
 import socket
+import logging
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def get_client_name(client_ip):
+    try:
+        with open('clients.txt', 'r') as file:
+            for line in file:
+                ip, name = line.strip().split(',')
+                if ip == client_ip:
+                    return name
+    except FileNotFoundError:
+        pass
+    return None
 
-msg = ''
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+def start_server(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(msg)
+    while True:
+        try:
+            sock.bind((host, port))
+            break
+        except OSError:
+            logging.info(f'Порт {port} уже занят, пробуем следующий порт...')
+            port += 1
 
-conn.close()
+    sock.listen(1)
+    logging.info(f'Сервер запущен на {host}:{port}')
+    print(f'Сервер запущен на порту {port}')
+
+    while True:
+        logging.info('Ожидание подключения клиента...')
+        client_socket, client_address = sock.accept()
+        client_ip = client_address[0]
+        logging.info(f'Подключен клиент {client_address}')
+
+        client_name = get_client_name(client_ip)
+
+        if not client_name:
+            client_socket.sendall('Введите имя: '.encode())
+            client_name = client_socket.recv(1024).decode().strip()
+            logging.info(f'Клиенту {client_address} присвоено имя {client_name}')
+
+            with open('clients.txt', 'a') as file:
+                file.write(f'{client_ip},{client_name}\n')
+
+        welcome_message = f'Добро пожаловать, {client_name}!'
+        client_socket.sendall(welcome_message.encode())
+
+        while True:
+            data = client_socket.recv(1024)
+
+            input_data = data.decode()
+            logging.info(f'{client_name}: {input_data}')
+
+            if input_data.strip().lower() == 'exit':
+                break
+
+            client_socket.sendall(data)
+            logging.info(f'{client_name}: {input_data}')
+
+        logging.info('Клиент отключен')
+        client_socket.close()
+
+    sock.close()
+
+IP = input("Введите IP-адрес сервера: ")
+port = int(input("Введите порт для внешнего подключения: "))
+
+start_server(IP, port)
