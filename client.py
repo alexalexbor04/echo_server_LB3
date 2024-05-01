f0a87fe159f1320884a8551332f22c64928c7d40
mishaklok <vbif.hfcjd@gmail.com>
2024-05-01 19:51:43 +0300

    Client connection added

--- client.py
+++ client.py
@@ -1,16 +1,23 @@
 import socket
-from time import sleep
+# from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+# sock = socket.socket()
+# sock.setblocking(1)
+# sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+# #msg = input()
+# msg = "Hi!"
+# sock.send(msg.encode())
 
-data = sock.recv(1024)
+# data = sock.recv(1024)
 
-sock.close()
+# sock.close()
 
-print(data.decode())
+# print(data.decode())
+
+IP = input('Введите ip адрес для подключения: ')
+port = int(input('Введите порт для подключения: '))
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, port))
+print(f'Соединение с {IP}:{port} успешно установлено')
