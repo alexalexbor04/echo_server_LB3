583ab24d83a0deec64009934503f139e7a82c327
mishaklok <vbif.hfcjd@gmail.com>
2024-05-01 20:04:01 +0300

    Добавлено завершение сессии и вод текста

--- client.py
+++ client.py
@@ -21,3 +21,19 @@ port = int(input('Введите порт для подключения: '))
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, port))
 print(f'Соединение с {IP}:{port} успешно установлено')
+
+try:
+    while True:
+
+        #обмен с сервером
+
+        message = input('Введите текст для отправки: ')
+
+        #обмен с сервером
+
+        if message == "exit":
+            break
+except:
+    print("Возникола непредвиденная ошибка, попробуйте снова.")
+    client_socket.close()
+client_socket.close()
